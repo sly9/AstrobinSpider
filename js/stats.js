360e5a175a6970aad1e40c36b080e1d498974de0
Liuyi Sun <sly.thu@gmail.com>
2021-07-25 19:08:21 -0700

    working 'stats.html' demo.
    run with a local http server

--- js/stats.js
+++ js/stats.js
@@ -1,36 +1,236 @@
-chart = {
-  const svg = d3.create("svg")
-      .attr("viewBox", [0, 0, width, height]);
+// image filename => image data
+let images = {}
+    // pyongc id => target details
+let topTargets = {}
 
-  svg.append("g")
-      .call(xAxis);
+async function loadData() {
+    const [imagesJSON, topTargetsJSON] = await Promise.all([
+        fetch('images.json'),
+        fetch('top_targets.json'),
+    ]);
+    images = await imagesJSON.json();
+    topTargets = await topTargetsJSON.json();
+    //console.log(images)
+    //console.log(topTargets)
 
-  svg.append("g")
-      .call(yAxis);
+    drawMarker(100);
 
-  svg.append("g")
-      .call(grid);
+}
 
-  const circle = svg.append("g")
-      .attr("stroke", "black")
-    .selectAll("circle")
-    .data(dataAt(1800), d => d.name)
-    .join("circle")
-      .sort((a, b) => d3.descending(a.population, b.population))
-      .attr("cx", d => x(d.income))
-      .attr("cy", d => y(d.lifeExpectancy))
-      .attr("r", d => radius(d.population))
-      .attr("fill", d => color(d.region))
-      .call(circle => circle.append("title")
-        .text(d => [d.name, d.region].join("\n")));
+let currentTargets = [];
 
-  return Object.assign(svg.node(), {
-    update(data) {
-      circle.data(data, d => d.name)
-          .sort((a, b) => d3.descending(a.population, b.population))
-          .attr("cx", d => x(d.income))
-          .attr("cy", d => y(d.lifeExpectancy))
-          .attr("r", d => radius(d.population));
+let currentTargetID = null;
+
+function drawMarker(n) {
+    currentTargets = moreFrequentThanXTimesTargets(n);
+    console.log(currentTargets);
+    var pointStyle = {
+        stroke: "#f0f",
+        width: 3,
+        fill: "rgba(255, 204, 255, 0.4)"
+    };
+
+    var currentPointStyle = {
+        stroke: "#f00",
+        width: 5,
+        fill: "rgba(255, 204, 255, 1)"
+    };
+
+    var textStyle = {
+        fill: "#f0f",
+        font: "bold 15px Helvetica, Arial, sans-serif",
+        align: "left",
+        baseline: "bottom"
+    };
+    Celestial.add({
+        type: "raw",
+        callback: function(error, json) {
+            console.log('first callback');
+            Celestial.redraw();
+        },
+
+        redraw: function() {
+
+            var m = Celestial.metrics(), // Get the current map size in pixels
+                // empty quadtree, will be used for proximity check
+                quadtree = d3.geom.quadtree().extent([
+                    [-1, -1],
+                    [m.width + 1, m.height + 1]
+                ])([]);
+
+            Object.values(currentTargets).forEach(target => {
+                // [RA, DEC] in degrees
+                let coordinatesDegree = target.coordinates['radians coords'].map(x => x * 180 / Math.PI);
+
+
+                if (Celestial.clip(coordinatesDegree)) {
+                    // get point coordinates
+                    var pt = Celestial.mapProjection(coordinatesDegree);
+                    // object radius in pixel, could be varable depending on e.g. dimension or magnitude 
+
+                    // Fake radius
+                    //var r = Math.pow(20 - prop.mag, 0.7); // replace 20 with dimmest magnitude in the data
+                    let r = Math.pow(Math.sqrt(target.astrobin_filenames.length), 0.7);
+
+                    // draw on canvas
+                    //  Set object styles fill color, line color & width etc.
+                    if (target.id == currentTargetID) {
+                        Celestial.setStyle(currentPointStyle);
+                    } else {
+                        Celestial.setStyle(pointStyle);
+                    }
+
+
+                    // Start the drawing path
+                    Celestial.context.beginPath();
+                    // Thats a circle in html5 canvas
+                    Celestial.context.arc(pt[0], pt[1], r, 0, 2 * Math.PI);
+                    // Finish the drawing path
+                    Celestial.context.closePath();
+                    // Draw a line along the path with the prevoiusly set stroke color and line width      
+                    Celestial.context.stroke();
+                    // Fill the object path with the prevoiusly set fill color
+                    Celestial.context.fill();
+
+                    // Set text styles       
+                    Celestial.setTextStyle(textStyle);
+                    // and draw text on canvas
+                    //Celestial.context.fillText(target.name, pt[0] + r - 1, pt[1] - r + 1);
+                }
+            });
+        }
+    });
+
+    Celestial.redraw();
+}
+
+function getClickPosition(e) {
+    var map = document.getElementById('celestial-map');
+    var x = e.offsetX;
+    var y = e.offsetY;
+    var inv = Celestial.mapProjection.invert([x, y]);
+    if (inv === undefined || isNaN(inv[0]))
+        return;
+
+    let closestTarget = closestTargetAt(inv[0], inv[1]);
+    console.log('clicking at: ', inv, closestTarget);
+    selectTarget(closestTarget);
+    // update slider show
+}
+
+function selectTarget(target) {
+    if (!target) return;
+
+    currentTargetID = target.id;
+
+    updateInfo(target);
+    updateSlider(target);
+}
+
+function updateInfo(target) {
+    $('#name').text(target.name);
+    $('#type').text(target.type);
+    $('#ra').text(target.coordinates['right ascension']);
+    $('#dec').text(target.coordinates.declination);
+
+    let otherName = '';
+    if (target['other identifiers']['common names']) otherName += target['other identifiers']['common names'] + ', ';
+    if (target['other identifiers'].messier) otherName += target['other identifiers'].messier + ', ';
+    $('#other_name').text(otherName)
+}
+
+function updateSlider(target) {
+    let relatedImages = astroBinImagesByID(target.id);
+    console.log(relatedImages);
+    relatedImages.sort(() => Math.random() - 0.5)
+
+
+    let sliderContainer = $('.slider')
+    sliderContainer.empty();
+    let html = ['<ul class="lightSlider" id="lightSlider">']
+
+    let len = relatedImages.length > 20 ? 20 : relatedImages.length;
+    for (let i = 0; i < len; i++) {
+        let img = relatedImages[i];
+        if (img.hash) {
+            html.push(`<li data-thumb="${img.url_thumb}"><a target="_blank" href="https://astrobin.com/${img.hash}/"><img src="${img.url_regular}" /></a></li>`)
+        } else {
+            html.push(`<li data-thumb="${img.url_thumb}"><a target="_blank" href="https://astrobin.com/${img.id}/"><img src="${img.url_regular}" /></a></li>`)
+        }
     }
-  });
+
+    html.push('</ul>');
+    sliderContainer.html(html.join(''));
+    $('#lightSlider').lightSlider({
+        gallery: true,
+        item: 1,
+        loop: true,
+        slideMargin: 0,
+        thumbItem: 9
+    });
+
+}
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+    loadData();
+    document.getElementById('celestial-map').addEventListener('click', getClickPosition, false);
+    $('#lightSlider').lightSlider({
+        gallery: true,
+        item: 1,
+        loop: true,
+        slideMargin: 0,
+        thumbItem: 9
+    });
+});
+
+function astroBinImagesByID(id) {
+    let targetDetails = topTargets[id];
+    let filenames = targetDetails['astrobin_filenames'];
+    return filenames.map(x => images[x]);
+}
+
+function moreFrequentThanXTimesTargets(x) {
+    let result = {};
+    Object.entries(topTargets).forEach(entry => {
+        let value = entry[1];
+        if (parseInt(value['count']) >= x) {
+            result[entry[0]] = entry[1];
+        }
+    });
+    return result;
+}
+
+function distance(ra0, dec0, ra1, dec1) {
+    return Math.sqrt(Math.pow(ra0 - ra1, 2) + Math.pow(dec0 - dec1, 2));
+}
+
+function closestTargetAt(ra, dec) {
+    if (ra < 0) ra = ra + 360;
+
+    let answer = null;
+    let bestDistance = Number.MAX_VALUE;
+    for (let key in currentTargets) {
+        let target = currentTargets[key];
+        let coordinatesDegree = target.coordinates['radians coords'].map(x => x * 180 / Math.PI);
+        let d = distance(ra, dec, coordinatesDegree[0], coordinatesDegree[1]);
+
+        if (d < bestDistance) {
+            bestDistance = d;
+            answer = target;
+        }
+    }
+
+    let answerCoords = answer.coordinates['radians coords'].map(x => x * 180 / Math.PI);
+
+    if (bestDistance > 5) {
+        //     console.log('The best answer I found is at: ', answer, answerCoords, ' but its too far', bestDistance);
+        return null;
+    } else {
+        //       console.log('The best answer I found is: ', answer, answerCoords, ' pretty close: ', bestDistance);
+    }
+    currentTargetID = answer.id;
+    Celestial.redraw();
+    return answer;
 }
--- most_frequent_targets.py
+++ most_frequent_targets.py
@@ -15,21 +15,24 @@ for i in images:
     if 'subjects' in image and len(image['subjects'])>0:
         subjects.extend(image['subjects'])
     
-x = Counter(subjects)
-#print(x)
+counts = Counter(subjects)
+print('aaa')
+#print(counts)
 #most_common_targets = list(map(lambda x: x[0], x.most_common(4000)))
-most_common_targets = list(filter(lambda x: not x[0].startswith('The star'), x.most_common(4000)))
+most_common_targets = {k: v for k, v in counts.items() if not k.startswith('The star')}
 
-print(most_common_targets)
+#most_common_targets = filter(lambda x: not x.startswith('The star'), counts)
+
+#print('length of most common targets: ',len(most_common_targets))
+#print('most_common_targets: ', most_common_targets)
 
 # key is pyongc id, value is (pyongc target)
 deduped_targets = {}
 
 failed_list = []
 json_list = []
-for t in most_common_targets:
-    name = t[0]
-    count = t[1]
+for name in most_common_targets:
+    count = most_common_targets[name]
     print('Solving %s with count %d...' % (name,count))
     if name == 'Other' or name == 'Comet':
         continue
@@ -43,14 +46,45 @@ for t in most_common_targets:
             json_obj['count'] = count
             json_list.append(json_obj)
     else:
-        print('Parsing %s failed' % name)
+        #print('Parsing %s failed' % name)
         failed_list.append(name)
 
-print(deduped_targets)
+#print(deduped_targets)
 #print(failed_list)
+
+
+### then fill image ids into DSO ids, so that we can refer it back later.
+
+target_dict = {}
+
+for dso in json_list:
+    target_dict[dso['id']] = dso
+
+#print(len(target_dict.keys()))
+#print(target_dict.keys())
+
+for image_filename in images:
+    image = images[image_filename]
+    if 'subjects' in image and len(image['subjects'])>0:
+        for s in image['subjects']:
+            dso = pyongc.get(s)
+            #try:
+            if dso:
+                real_dso = target_dict[dso.id]
+                if 'astrobin_filenames' not in real_dso:
+                    real_dso['astrobin_filenames'] = []
+                real_dso['astrobin_filenames'].append(image_filename)
+            #except:
+            #    print('Parsing this failed... ')
+            #    print(pyongc.printDetails(dso))
+
+
 
 with open('parsing_failed_targets.txt','w') as f:
     for t in failed_list:
         f.write('%s\n'%t)
-with open('parsed_subjects.json','w') as f:
-    json.dump(json_list, f)
+#with open('parsed_subjects.json','w') as f:
+#    json.dump(json_list, f)
+
+with open('top_targets.json','w') as f:
+    json.dump(target_dict, f)
